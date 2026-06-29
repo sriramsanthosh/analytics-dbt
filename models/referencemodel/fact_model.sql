0571c6e9e2a1c89b092b5dd94848efb037479e6a
Santhosh Itha <santitha@cisco.com>
2026-06-29 06:11:58 +0000

    errors fixed

--- models/referencemodel/fact_model.sql
+++ models/referencemodel/fact_model.sql
@@ -6,9 +6,9 @@ select
     c.last_name,
     c.email,
     count(o.order_id)                               as total_orders,
-    -- sum(o.amount)                                   as total_spent,
-    -- sum(case when o.order_tier = 'high'
-    --     then 1 else 0 end)                          as high_value_orders,
+    sum(o.amount)                                   as total_spent,
+    sum(case when o.order_tier = 'high'
+        then 1 else 0 end)                          as high_value_orders,
     min(o.order_date)                               as first_order_date,
     max(o.order_date)                               as last_order_date
 from {{ ref('stg_orders') }} c
